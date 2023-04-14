3de6759b67eac02e7a0b97f4281d4ef8975dfa5d
Mahmoud Shalaby <mhmooud074@gmail.com>
2023-04-14 23:19:50 +0200

    Added owner, session scanner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps, session }: AppProps) {
   return (
     <SessionProvider session={session} >
       <div className="bg-slate-900 h-screen grid place-items-center text-white">
+        <Head>
+          <title>Dross</title>
+        </Head>
         <Component {...pageProps} />
       </div>
     </SessionProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,32 @@
-import Head from 'next/head'
 import { getServerSession } from 'next-auth/next'
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import { authOptions } from "./api/auth/[...nextauth]"
+import { PrismaClient } from '@prisma/client'
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getServerSession(context.req, context.res, authOptions)
   if (!session)
     return { redirect: { destination: '/auth/login' } }
-  return { props: { session }}
+
+  const prisma = new PrismaClient()
+  const stages = await prisma.stage.findMany({
+    where: {
+      owner: session.user?.email
+    }
+  })
+
+  return { props: { session, stages } }
 }
 
-export default function Home({ session }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return (
-    <div>
-      <Head>
-        <title>Dross manager</title>
-      </Head>
-      {session ? (
-        <h1 className="text-6xl">Dross manager</h1>
-      ):(
-        <h1 className="text-6xl md:text-8xl">Dross</h1>
-      )}
-    </div>
-  )
+export default function Home({ session, stages }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(stages)
+  if (session)
+    return (
+      <div>
+        <h1 className="text-3xl">{session.user?.name}'s email is {session.user?.email}</h1>
+        {session.user?.image ? <img src={session.user?.image} alt="" />:''}
+      </div>
+    )
+
+  return <h1 className="text-6xl md:text-8xl">Dross</h1>
 }
