d87926440ee4c7dc702f8c6bb4c57866b69a0d81
Mahmoud Shalaby <mhmooud074@gmail.com>
2023-04-15 04:29:02 +0200

    removed access to accounts name and image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,9 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import Head from 'next/head'
 
-export default function App({ Component, pageProps, session }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
   return (
-    <SessionProvider session={session} >
+    <SessionProvider session={session}>
       <div className="bg-slate-900 h-screen grid place-items-center text-white">
         <Head>
           <title>Dross</title>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,26 @@ import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'nex
 import { authOptions } from "./api/auth/[...nextauth]"
 import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
+
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getServerSession(context.req, context.res, authOptions)
-  if (!session?.user?.email)
+  const email = session?.user?.email
+  if (!email)
     return { redirect: { destination: '/auth/login' } }
 
-  const prisma = new PrismaClient()
   const stages = await prisma.stage.findMany({
-    where: { owner: session.user?.email }
+    where: { owner: email }
   })
 
-  return { props: { session, stages } }
+  return { props: { email, stages } }
 }
 
-export default function Home({ session, stages }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(stages)
-  if (session)
+export default function Home({ email, stages }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (email)
     return (
       <div>
-        <h1 className="text-3xl">{session.user?.name}'s email is {session.user?.email}</h1>
-        {session.user?.image ? <img src={session.user?.image} alt="" />:''}
+        <h1 className="text-3xl">{email}</h1>
       </div>
     )
 
